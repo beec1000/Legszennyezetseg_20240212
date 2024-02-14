58862b7b9ceb5d760087d514bde104ac11792f5c
beec1000 <beec1000.minecraft@gmail.com>
2024-02-14 11:22:22 +0100

    Add project files.

---
+++ Legszegenyseg_20240212/Legszennyezettseg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legszegenyseg_20240212
+{
+    internal class Legszennyezettseg
+    {
+        public int Napok { get; set; }
+        public List<int> Orak { get; set; }
+
+        public Legszennyezettseg(int n, string s)
+        {
+            string[] v = s.Split(';');
+            this.Napok = n;
+            this.Orak = new List<int>();
+            for (int i = 0; i < v.Length; i++)
+            {
+                Orak.Add(int.Parse(v[i]));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Napok}. {string.Join(" ", this.Orak)}";
+        }
+    }
+}
---
+++ Legszegenyseg_20240212/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Legszegenyseg_20240212
+{
+    internal class Program
+    {
+        static Legszennyezettseg F3(List<Legszennyezettseg> l)
+        {
+            var x = l.MaxBy(d => d.Orak.Max());
+            
+            return x;
+
+        }
+
+        static int F4(List<Legszennyezettseg> l)
+        {
+            var x = l.SelectMany(d => d.Orak).Max();
+            return x;
+        }
+
+        static int F5(List<Legszennyezettseg> l) => l.Sum(d => d.Orak.Where(d => d < 100).Count());
+
+
+        static double F6(List<Legszennyezettseg> l) => l.SelectMany(d => d.Orak).Average();
+
+        //static IEnumerable<int> F7(List<Legszennyezettseg> l)
+        //{
+        //    var x = l.Select(d => d.Orak);
+        //    return x;
+        //}
+
+        static void Main(string[] args)
+        {
+            List<Legszennyezettseg> szennyezettseg = new List<Legszennyezettseg>();
+            using StreamReader sr = new StreamReader(@"..\..\..\src\SO2.txt");
+            for (int i = 1; !sr.EndOfStream; i++)
+            {
+                szennyezettseg.Add(new Legszennyezettseg(i, sr.ReadLine()));
+            }
+
+            //2. feladat
+            foreach (var i in szennyezettseg)
+            {
+                Console.WriteLine(i);
+            }
+
+            //3. feladat
+            using StreamWriter sw = new StreamWriter(@"..\..\..\src\UjSO2.txt");
+            sw.WriteLine($"{F3(szennyezettseg).Napok} |");
+
+
+
+            Console.WriteLine("4. feladat");
+            Console.WriteLine($"A legmagasabb SO2 tartalom {F4(szennyezettseg)} volt.");
+
+            Console.WriteLine("5. feladat");
+            Console.WriteLine($"Összesen {F5(szennyezettseg)} olyan óra volt.");
+
+            Console.WriteLine("6. feladat");
+            Console.WriteLine($"Az átlagos SO2 tartalom a hónapban: {Math.Round(F6(szennyezettseg), 4)}");
+
+            Console.WriteLine("7. feladat");
+            //if (F7(szennyezettseg) <= 60)
+            //{
+
+            //}
+            //Console.WriteLine($"{}");
+
+        }
+    }
+}
